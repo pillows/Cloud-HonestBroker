aeee905edfb5f45d9eb651029c3b48b46cca13b0
Matthew Wong <matthew.wong@outlook.com>
2019-05-11 21:32:03 -0700

    Finished displaying jobs table

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 
 app.use(bodyParser.json());
 app.disable('etag');
@@ -30,42 +31,46 @@ app.get('/network-admin', function (req, res) {
 
 app.get('/network-stats', function (req, res) {
 
-    let status =
-        [
-            [{
-                "domain":"domain1",
-                "vms":[
-                    {"name":"vm1", "busy":true},
-                    {"name":"vm2", "busy":true},
-                    {"name":"vm3", "busy":false}
-                ],
-                "layers":[0.25, 0.1]
-            },
-            {
-                "domain":"domain2",
-                "vms":[
-                    {"name":"vm4", "busy":true},
-                    {"name":"vm5", "busy":true},
-                    {"name":"vm6", "busy":false}
-                ],
-                "layers":[0.25, 0.1]
-            },
-            {
-                "domain":"domain3",
-                "vms":[
-                    {"name":"vm7", "busy":true},
-                    {"name":"vm8", "busy":true},
-                    {"name":"vm9", "busy":false}
-                ],
-                "layers":[0.25, 0.1]
-            }
-        ],
-        [
-            {"job_owner":"Shelly", "job_name":"Bio lab results", "job_size":"500MB", "job_timestamp":"2344123123"}
-        ]
+    let rawdata = fs.readFileSync('status.json');
+    let status = JSON.parse(rawdata);
 
 
-        ]
+    // let status =
+    //     [
+    //         [{
+    //             "domain":"domain1",
+    //             "vms":[
+    //                 {"name":"vm1", "busy":true},
+    //                 {"name":"vm2", "busy":true},
+    //                 {"name":"vm3", "busy":false}
+    //             ],
+    //             "layers":[0.25, 0.1]
+    //         },
+    //         {
+    //             "domain":"domain2",
+    //             "vms":[
+    //                 {"name":"vm4", "busy":true},
+    //                 {"name":"vm5", "busy":true},
+    //                 {"name":"vm6", "busy":false}
+    //             ],
+    //             "layers":[0.25, 0.1]
+    //         },
+    //         {
+    //             "domain":"domain3",
+    //             "vms":[
+    //                 {"name":"vm7", "busy":true},
+    //                 {"name":"vm8", "busy":true},
+    //                 {"name":"vm9", "busy":false}
+    //             ],
+    //             "layers":[0.25, 0.1]
+    //         }
+    //     ],
+    //     [
+    //         {"job_owner":"Shelly", "job_name":"Bio lab results", "job_size":"500MB", "job_timestamp":"2344123123"}
+    //     ]
+    //
+    //
+    //     ]
 
     res.status(200).json(status)
 });
